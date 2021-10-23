ecd1e333d3e89039434161c937c335e1f827fc6c
umarcio <u.mc.oliveira@gmail.com>
2021-10-23 19:27:24 -0300

    Correção do programa de vetor da aula

--- Vetor_3/main.c
+++ Vetor_3/main.c
@@ -3,39 +3,19 @@
 
 
 int main(){
-    int a;
-    double soma, media;
-
-    printf("Quantos numeros voce vai digitar? ");
-    scanf("%d", &a);
+	
+	int i;
+    printf("Voce deve digitar treis numeros: \n");
 
     double vetor[3];
+    
 
-    for (int i=0; i<3; i++) {
+    for (i =0; i<3; i++) {
         printf("Digite um numero: ");
         scanf("%lf", &vetor[i]);
     }
+       printf("O Vetor digitado contem : [%.lf,%.lf,%.lf]\n", vetor[0],vetor[1],vetor[2]);
 
-	//soma = 0;
-   // for (int i=0; i<n; i++) {
-       // soma = soma + vetor[i];
-   // }
-
-   // media = soma / n;
-
-	//printf("VALORES = ");
-
-    //for (int i=0; i<n; i++) {
-        //printf("%.1lf  ", vetor[i]);
-    //}
-
-   // printf("\nSOMA = %.2lf\n", soma);
-	//printf("MEDIA = %.2lf\n", media);
 
     return 0;
 }
-
-
-
-
-
